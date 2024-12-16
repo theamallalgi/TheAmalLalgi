9b31623799862132ae886fd29ba40d37dc5ef4bc
Amal Lalgi <amallalgi2487@gmail.com>
2024-12-16 18:45:18 +0530

    fix: change file finding alg.

--- scripts/update-quote.py
+++ scripts/update-quote.py
@@ -5,11 +5,10 @@ import random
 
 # Get the directory of the current script
 script_dir = os.path.dirname(os.path.abspath(__file__))
-data_dir = os.path.join(script_dir, 'data')  # Navigate to the 'data' folder
-json_file_path = os.path.join(data_dir, 'quotes.json')
+data_file_path = os.path.join(script_dir, 'data', 'quotes.json')
 
 # Load the JSON file with quotes
-with open(json_file_path, 'r') as file:
+with open(data_file_path, 'r') as file:
     data = json.load(file)
 
 # Get today's date in MM-DD format
@@ -19,8 +18,8 @@ today = datetime.datetime.now().strftime('%m-%d')
 special_message = data["special_days"].get(today)
 quote = special_message if special_message else random.choice(data["random_quotes"])
 
-# Update README.md
-readme_path = os.path.join(script_dir, '..', '..', 'README.md')  # Navigate up two levels to find README.md
+# Update README.md located in the repository root
+readme_path = os.path.join(script_dir, '..', '..', 'README.md')
 with open(readme_path, 'r+') as readme:
     lines = readme.readlines()
     lines[4] = f"> {quote}\n"  # Replace the 5th line (index 4)
