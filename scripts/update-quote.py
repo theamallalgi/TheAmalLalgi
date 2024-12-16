79cbc3cdc5a0cbac3412ca15a63c89c0790ea3f4
Amal Lalgi <amallalgi2487@gmail.com>
2024-12-16 18:53:21 +0530

    fix: account to file path change

--- scripts/update-quote.py
+++ scripts/update-quote.py
@@ -1,15 +1,9 @@
-import os
 import json
 import datetime
 import random
 
-# Get the directory of the current script
-script_dir = os.path.dirname(os.path.abspath(__file__))
-# data_file_path = os.path.join(script_dir, 'data', 'quotes.json')
-data_file_path = '/home/runner/work/theamallalgi/theamallalgi/scripts/data/quotes.json'
-
 # Load the JSON file with quotes
-with open(data_file_path, 'r') as file:
+with open('quotes.json', 'r') as file:
     data = json.load(file)
 
 # Get today's date in MM-DD format
@@ -19,9 +13,8 @@ today = datetime.datetime.now().strftime('%m-%d')
 special_message = data["special_days"].get(today)
 quote = special_message if special_message else random.choice(data["random_quotes"])
 
-# Update README.md located in the repository root
-readme_path = os.path.join(script_dir, '..', '..', 'README.md')
-with open(readme_path, 'r+') as readme:
+# Update README.md
+with open('README.md', 'r+') as readme:
     lines = readme.readlines()
     lines[4] = f"> {quote}\n"  # Replace the 5th line (index 4)
     readme.seek(0)
